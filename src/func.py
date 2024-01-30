e5fc03daed38397f52497bc10d8e33909a9d62fc
tristenwallace <tri3sten@gmail.com>
2024-01-30 13:50:30 -0500

    feat: Impute nan with linear regression
    is_linear_relationship function checks if we can use linear regression
    to impute missing values. 'weekly_price' nan values are imputed this
    way.
    
    We also organized some data wrangling headers.

---
+++ src/func.py
@@ -0,0 +1,30 @@
+import statsmodels.api as sm
+import pandas as pd
+
+def is_linear_relationship(df, parameters, target, thresh=.5):
+    '''
+    INPUT
+        df (pandas dataframe)
+        parameters (list of str): List of column names to predict target
+        target (str): target column name 
+        thresh (float): minimum r-squared value to consider imputing w/ linear reg
+    
+    OUTPUT
+        Prints r squared value and whether to use linear regression to impute missing values.
+        Does not return a value
+    '''
+
+    # drop NaN values    
+    df = df.dropna()
+
+    y = df[target]
+    X = df[parameters]
+    X['intercept'] = 1
+
+    lm = sm.OLS(y, X)
+    r = lm.fit().rsquared_adj
+    
+    if r>thresh:
+        print("R-squared of {} is {}. Impute na w/ linear regression.".format(target, r))
+    else:
+        print("R-squared of {} is {}. Impute na w/ mean or drop column.".format(target, r))
